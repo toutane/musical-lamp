8257b13c8e103bf41e2599022f1f4fe9812500e9
toutane <ca@leger.email>
2021-12-03 22:03:46 +0100

    Setup webservices (cookie management) + login in unity

---
+++ musical-lamp-game/Assets/Scripts/Backend/Webservices.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+
+public class Webservices : MonoBehaviour
+{
+    static string baseURL = "http://localhost:3000";
+
+    public static string CookieString {
+        get {
+            return PlayerPrefs.GetString("cookie");
+        }
+        set {
+            PlayerPrefs.SetString("cookie", value);
+        }
+    }
+
+    public static string BuildUrl(string path) {
+        return Path.Combine(baseURL, path).Replace(Path.DirectorySeparatorChar, '/');
+    }
+
+    public static UnityWebRequest Get(string path) {
+        var request = new UnityWebRequest(BuildUrl(path), "GET");
+        if (!string.IsNullOrEmpty(CookieString)) {
+            request.SetRequestHeader("cookie", CookieString);
+        }
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        return request;
+    }
+
+    public static UnityWebRequest Post(string path, string jsonString) {
+        var request = new UnityWebRequest(BuildUrl(path), "POST");
+        if (!string.IsNullOrEmpty(CookieString)) {
+            request.SetRequestHeader("cookie", CookieString);
+        }
+        byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(jsonString);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        return request;
+    }
+}
